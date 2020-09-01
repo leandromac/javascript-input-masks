de858a1ecb7b610154c9ebb0f504d1ffe25c6bc0
leandromac <contato@leandromacedo.com>
2020-09-01 17:01:39 -0300

    Date mask

--- masks.js
+++ masks.js
@@ -37,7 +37,14 @@ const masks = {
     .replace(/(\d{5})(\d)/,'$1.$2')
     .replace(/(\d{5}\.)(\d{2})(\d)/,'$1$2-$3')
     .replace(/(-\d{1})\d+?$/, '$1')
-  }
+  },
+  date(value) {
+    return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/,'$1/$2')
+    .replace(/(\d{2})(\d)/,'$1/$2')
+    .replace(/(\d{4})\d+?$/, '$1')
+  },
 };
 
 document.querySelectorAll('input').forEach(($input) => {
